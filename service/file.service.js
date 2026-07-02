4b327ddc2da147cd67e477d87e9ad8ae1de5a1ed
aviv deri <aviv322deri@gmail.com>
2026-07-02 12:15:57 +0300

    add file.cervice.js

---
+++ service/file.service.js
@@ -0,0 +1,22 @@
+import fs from 'fs'
+
+const fileName = "../data/movies.json"
+
+export function readFileMovies(callback) {
+    fs.readFile(fileName, "utf8", (err, data) => {
+        if (err) return err.message
+        try {
+            callback(JSON.parse(data))
+        } catch (err) {
+            console.log("file content is not a Json")
+        }
+    })
+}
+readFileMovies(console.log)
+
+export function writeFileMovies(content) {
+    fs.writeFile(fileName, JSON.stringify, content, (err) => {
+        if (err) return err.message
+        console.log("succsses")
+    })
+}
